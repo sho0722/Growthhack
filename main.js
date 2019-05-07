4ca1e33f23f61bb185d3d1b42c5bf2056bd89bfb
Sho Tanaka <swallowtail.722.kyoto@gmail.com>
2019-05-07 11:17:07 +0900

    Webページ作成

---
+++ main.js
@@ -0,0 +1,22 @@
+$('h1').hide().fadeIn(4000);
+
+$("#A").click(function(){
+    $("#Analyticity").fadeIn(1500);
+});
+
+$("#B").click(function(){
+    $("#Broad-interest").fadeIn(1500);
+});
+
+$("#C").click(function(){
+    $("#Creative").fadeIn(1500);
+});
+
+$("#D").click(function(){
+    $("#Discipline").fadeIn(1500);
+});
+
+$("#E").click(function(){
+    $("#Empathy").fadeIn(1500);
+});
+
